53081f41391aa82874ddb8244fedc9a332474440
Alistaire Noiprasit <alistaire.noiprasit@gmail.com>
2025-11-11 13:40:57 -0500

    Add new module stubs

---
+++ calc_binding_alistaire_noiprasit/file_calc/file_calculator.py
@@ -0,0 +1,8 @@
+#
+# Created by Alistaire Noiprasit on 11/11/2025.
+#
+from calc_binding_alistaire_noiprasit.calculator import Calculator
+
+class FileCalculator(Calculator):
+    def sum_file(self, path):
+        raise NotImplementedError
